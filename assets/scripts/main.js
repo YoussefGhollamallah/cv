ced358fa02696a4cd04b18bec4b2561be1be9abf
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-20 23:57:56 +0200

    Création du fichier main : mise en place de l'affichage des h2

---
+++ assets/scripts/main.js
@@ -0,0 +1,11 @@
+let job = document.getElementById("job");
+job.innerText = "Développeur web et web mobile.".toUpperCase();
+
+let about = document.getElementById("aboutTitle")
+about.innerText = "à propos.".toUpperCase();
+
+let experienceTitle = document.getElementById("titleExperience")
+experienceTitle.innerText = "expériences".toUpperCase();
+
+let skillTitle = document.getElementById("titleSkills")
+skilleTitle.innerText = "skills".toUpperCase();
