efbc80647b3025db7666cb49725233c633a963e6
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-21 00:00:51 +0200

    import des includes, du ficher css et js

--- index.php
+++ index.php
@@ -3,12 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/style.css">
     <title>Document</title>
+    <script src="assets/scripts/main.js" defer></script>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    
+    <?php require_once "includes/header.php"; ?>
+
+    <?php require_once "includes/main.php"; ?>
 
     <?php require_once "includes/footer.php"; ?>
+
 </body>
 </html>
