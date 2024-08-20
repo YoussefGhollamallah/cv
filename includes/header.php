212a8a6fe997ca0f256bdeeb81750d6340e615ab
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-21 00:13:12 +0200

    ajout de la nav

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,18 @@
 <header>
-    <section>
-        <h1>Youssef Ghollamallah</h1>
-        <p id="job"></p>
+    <section id="header">
+        <article>
+            <h1>Youssef Ghollamallah</h1>
+            <p id="job"></p>
+        </article>
+        <article>
+            <img id="profilPicture" src="assets/pictures/profil.png" alt="photo" width="200px" >
+        </article>
     </section>
-    <section>
-        <img id="profilPicture" src="assets/pictures/profil.png" alt="photo" width="200px" >
-    </section>
+    <nav>
+        <ul id="navlink">
+            <li><a href="+33750847599">+337.50.84.75.99</a></li>
+            <li><a href="mailto:youssef.gh@live.fr">youssef.gh@live.fr</a></li>
+        </ul>
+    </nav>
 </header>
 
