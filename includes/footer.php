4969497a1a5a64de8769eb387f0b1c0b7c8df2d7
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-20 16:56:01 +0200

    commit initial

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <section>
+        <article id="reseaux">
+            <h3>Social Media</h3>
+            <ul>
+                <li><a href="https://www.linkedin.com/in/youssef-ghollamallah/" target="_blank" rel="noopener noreferrer"><img class="reseaux" src="assets/pictures/linkedin.png" alt=""></a></li>
+                <li><a href="https://github.com/YoussefGhollamallah" target="_blank" rel="noopener noreferrer"><img class="reseaux" src="assets/pictures/github.png" alt=""></a></li>
+            </ul>
+        </article>
+        <article id="cv">
+            <h3>Mon Curriculum Vitae</h3>
+        </article>
+    </section>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<header>
+    <section>
+        <h1>Youssef Ghollamallah</h1>
+        <p>Développeur web et web mobile</p>
+    </section>
+    <section>
+        
+    </section>
+</header>
