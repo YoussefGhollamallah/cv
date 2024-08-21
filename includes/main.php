a7214c0fe47afeffa0d7e055029931da514ffa46
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-21 11:48:33 +0200

    ajout d'article

--- includes/main.php
+++ includes/main.php
@@ -1,24 +1,66 @@
 <main>
-    <article id="about">
-        <h2 id="aboutTitle">A PROPOS.</h2>
-        <p>Youssef Ghollamallah, développeur web.</p>
-        <p>Plusieurs années dans la restauration , patissier et cuisinier.</p>
-        <p>Esprit d'équipe, rigueur et écoute.</p>
-        <p>Période covid, reconvertion dans le développement web, domaine qui m'intéressait depuis des années.</p>
-        <p>Formation Bootcamp DWWM, Bachelor IT à la plateforme ou j'ai pu découvrir les métiers du numérique.</p>
-        <p>Alternance en CDPI en DWWM au sein de la plateforme.</p>
-    </article>
-
+    <section id="about">
+        <article >
+            <h2 id="aboutTitle"></h2>
+            <p>Youssef Ghollamallah, développeur web.</p>
+            <p>Plusieurs années dans la restauration , patissier et cuisinier.</p>
+            <p>Esprit d'équipe, rigueur et écoute.</p>
+            <p>Période covid, reconvertion dans le développement web, domaine qui m'intéressait depuis des années.</p>
+            <p>Formation Bootcamp DWWM, Bachelor IT à la plateforme ou j'ai pu découvrir les métiers du numérique.</p>
+            <p>Alternance en CDPI en DWWM au sein de la plateforme.</p>
+        </article>
+        <article id="artprofil">
+            <img id="profilPicture" src="assets/pictures/profil.png" alt="photo" width="200px" >
+        </article>
+    </section>
+        
     <article id="skills">
         <h2 id="titleSkills"></h2>
-    </article>
+        <div>
+            
+            <ul id="linkIcon">
+                <li>
+                    <a href=""><img class="icon" src="assets/pictures/css-3.png" alt="icon css">CSS</a>
+                </li>
+                <li>
+                    <a href=""><img class="icon" src="assets/pictures/html-5.png" alt="icon html">HTML</a>
+                </li>
+                <li>
+                    <a href=""><img class="icon" src="assets/pictures/js.png" alt="icon js">JS</a>
+                </li>
+                <li>
+                    <a href=""><img class="icon" src="assets/pictures/php.png" alt="icon php">PHP</a>
+                </li>
+                <li>
+                    <a href=""><img class="icon" src="assets/pictures/gite.png" alt="icon git">GIT</a>
+                </li>
+            </ul>
+        </div>
 
-    <article id="experiences">
-        <h2 id="titleExperience"></h2>
     </article>
-
-    <article id="formations">
+    
+    <section id="formations">
         <h2 id="titleFormation"></h2>
-    </article>
+        <article>
+            <h3></h3>
+        </article>
+    </section>
+    
+    <section id="experiences">
+        <h2 id="titleExperience"></h2>
+        <article>
+            <h3>Développeur Web et Web Mobile</h3>
+            <h4>La Plateforme | 08/2024-EN COURS</h4>
+        </article>
+        <article>
+            <h3>Commis de cuisine</h3>
+            <h4>Brasserie le kilt | 08/2017-08/2022</h4>
+        </article>
+        <article>
+            <h3>Pâissier</h3>
+            <h4>Boulagerie de Sainte-Marthe | 09/2012-08/2013</h4>
+        </article>
+    </section>
+
 
 </main>
