117c3b5efb8ce88515a3b29b682818680b68e80f
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-21 11:48:05 +0200

    modification du header

--- includes/header.php
+++ includes/header.php
@@ -4,13 +4,11 @@
             <h1>Youssef Ghollamallah</h1>
             <p id="job"></p>
         </article>
-        <article>
-            <img id="profilPicture" src="assets/pictures/profil.png" alt="photo" width="200px" >
-        </article>
+    
     </section>
     <nav>
         <ul id="navlink">
-            <li><a href="+33750847599">+337.50.84.75.99</a></li>
+            <li><a href="callto:+33750847599">07.50.84.75.99</a></li>
             <li><a href="mailto:youssef.gh@live.fr">youssef.gh@live.fr</a></li>
         </ul>
     </nav>
