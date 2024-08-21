3b95e32514f2ec0680b825ec6a23966cc32f7edb
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-21 11:48:51 +0200

    update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="assets/scripts/main.js" defer></script>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css?<?php $t = time(); ?>">
 </head>
 <body>
     <?php require_once "includes/header.php"; ?>
